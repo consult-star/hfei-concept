aadd7f50ae81f3f52cc7688d8848ac288c36f54a
consult-star <rubioaamir@gmail.com>
2025-06-20 23:12:34 +0100

    Update

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  basePath: isProd ? "/hfei-concept" : "",
+  basePath: "/hfei-concept",
   output: "export",
 };
 
